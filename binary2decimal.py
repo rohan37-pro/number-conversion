4534513fe27cd409a8bef912911759473abc9031
ROHAN <75291986+rohan37-pro@users.noreply.github.com>
2021-09-15 02:13:49 -0400

    Add files via upload

---
+++ binary2decimal.py
@@ -0,0 +1,37 @@
+class bin2dec :
+
+	def __init__(self):
+		self.binary = input("enter the binary : ")
+		self.left_part = self.binary.split(".")[0]
+		try:
+			self.right_part  = self.binary.split(".")[1]
+		except:
+			self.right_part = ''
+
+	def convert_right(self):
+		decimal = 0
+		pow = -1
+		for i in range(len(self.right_part)):
+			decimal += int(self.right_part[i])*(2**pow)
+			pow-=1
+		return decimal
+
+
+	def convert_left(self):
+		decimal = 0
+		pow = 0
+		for i in range(-1,-len(self.left_part)-1,-1):
+			decimal += int(self.left_part[i])*(2**pow)
+			pow+=1
+		return decimal
+
+
+while True:
+	mode = input("select mode- convert(c)/quit(q) : ")
+	if mode=="c":
+		convert = bin2dec()
+		decimal = convert.convert_left() + convert.convert_right()
+		print(f"decimal of binary {convert.binary} is {decimal}")
+	if mode=="q":
+		break
+		
